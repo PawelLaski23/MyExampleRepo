c82f3ea35cd67672b5f69ed7a11575ce2421bc4e
PawelGlosel <85611398+PawelLaski23@users.noreply.github.com>
2023-01-08 19:49:05 +0100

    add lombok to User class

--- src/test/java/Models/User.java
+++ src/test/java/Models/User.java
@@ -1,25 +1,11 @@
 package Models;
 
+import lombok.Data;
+@Data
 public class User {
     private int randomNumber = (int)Math.floor(Math.random()*100);
     private String name = "Pawel";
     private String lastName = "Laski";
     private String email = "pawellaski" + randomNumber + "@email2.pl";
     private String password = "Password123!";
-
-    public String getName() {
-        return name;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
 }
